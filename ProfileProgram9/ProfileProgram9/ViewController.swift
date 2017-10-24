bc0c1c80ce4c1b98e5a94abf548ea1bf7efa3fa7
Admin <vitaliy.kazakoff2013@gmail.com>
2017-10-24 10:03:48 +0300

    Creat Project

---
+++ ProfileProgram9/ProfileProgram9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProfileProgram9
+//
+//  Created by Admin on 24.10.2017.
+//  Copyright © 2017 Vitaliy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
